af4a186ef6fbb0d0d223f68733b3261c4cc992d0
Rupinder kaur Sandhu <rupinder61093sandhu@gmail.com>
2024-12-15 22:25:31 -0500

    first commit

---
+++ hooks/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+app.use(express.json());
+const client = new PrismaClient();
+
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body = req.body;
+
+  await client.$transaction(async (tx) => {
+    const run = await client.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body,
+      },
+    });
+
+    await client.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
+
+  res.json({
+    message: "Webhook received",
+  });
+  //store in db a new trigger
+  //push it on  to a queue(kafka/redis)
+});
+
+app.listen(3000);
